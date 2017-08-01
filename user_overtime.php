4e3fb9a851dc4b97f5eefeab56d04a2a74f75149
David Hsieh <davidhsieh7988@gmail.com>
2017-08-01 17:24:05 +0800

    Add files via upload

--- user_overtime.php
+++ user_overtime.php
@@ -10,10 +10,10 @@
 </head>
 <body>
 	<div class="top">
-		<a href="index.php">
+		<a href="user_index.php">
 			<img src="http://www.lctech.com.tw/wp-content/uploads/bfi_thumb/%E6%9C%AA%E5%91%BD%E5%90%8D-1-%E6%8B%B7%E8%B2%9D-31w65e1o8bhj94p9q5patm@2x.png" width="50" height="50" style="float:left;margin-left:15px">
 		</a>
-		<a href="profile.php">
+		<a href="user_profile.php">
 			<i class="material-icons" style="margin-top:15px;margin-right:15px;margin-left:5px;float:right;font-size:30px;color:#CCC">person</i>
 		</a>
 		<a href="#">
@@ -33,12 +33,12 @@
 				<p style="line-height:20px;border-right:solid 1px #CCC"></p>
 				<div>
 					<div class="left-title" >
-						<a href="introduction.php" style="margin:auto 20px">歡迎使用</a>
+						<a href="user_introduction.php" style="margin:auto 20px">歡迎使用</a>
 					</div>
 				</div>
 				<div>
 					<div class="left-title">
-						<a href="index.php" style="margin:auto 20px">行事曆</a>
+						<a href="user_index.php" style="margin:auto 20px">行事曆</a>
 					</div>
 				</div>
 				<div>
@@ -56,16 +56,16 @@
 						<p style="margin:auto 20px">表單申請</p>
 					</div>
 					<div  class="left-list">
-						<a href="leave.php" style="margin:auto 30px;color:#666666">請假申請</a>
+						<a href="user_leave.php" style="margin:auto 30px;color:#666666">請假申請</a>
 					</div>
 					<div class="left-list">
-						<a href="bTrip.php" style="margin:auto 30px;color:#666666">出差申請</a>
+						<a href="user_bTrip.php" style="margin:auto 30px;color:#666666">出差申請</a>
 					</div>
 					<div class="left-list">
 						<p style="margin:auto 30px;color:#666666">加班申請</p>
 					</div>
 					<div class="left-list">
-						<a href="#" style="margin:auto 30px;color:#666666">差勤明細</a>
+						<a href="user_bTrip_list.php" style="margin:auto 30px;color:#666666">差勤明細</a>
 					</div>
 				</div>
 				<div>
@@ -81,7 +81,7 @@
 				</div>
 				<div>
 					<div class="left-title">
-						<a href="#" style="margin:auto 20px">登出</a>
+						<a href="log_logout.php" style="margin:auto 20px">登出</a>
 					</div>
 				</div>
 			</div>
@@ -89,6 +89,18 @@
 		<div class="right">
 			<div class="right-top">
 				<p style="line-height:50px;font-family:Microsoft JhengHei;font-size:25px;margin:auto 15px">加班申請</p>
+			</div>
+			<div class="right-down">
+				<form style="margin-left:15px;font-family:Microsoft JhengHei;font-size:18px" method="post" action="insert_overtime.php">
+					<p>申請人：<?php echo $_SESSION['name']; ?></p>
+					<p>加班日期：<input type="date" name="o_date" size="8"></p>
+					<p>時間：<input  type="time" name="o_start" size="7"> to <input  type="time" name="o_end" size="7"></p>
+					<p>申請加班費時數：<input type="text" name="o_hrs" size="5"></p>
+					<p>申請補修時數：<input type="text" name="o_phrs" size="15"></p>
+					<p>加班事由：<input type="text" name="o_state" size="50" placeholder=" 請輸入詳細說明"></p>
+					<p>備註：<input type="text" name="o_comment" size="50"></p>
+					<input type="submit" name="" value="送出表單" size="6.5" class="btn">
+				</form>
 			</div>
 			
 		</div>
