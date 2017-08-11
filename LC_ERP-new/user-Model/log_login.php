f58a2397fcb41055f80667d81ce93507ddb49b0c
Stephanie <stefanychen10@gmail.com>
2017-08-11 12:03:00 +0800

    Add files via upload

--- LC_ERP-new/user-Model/log_login.php
+++ LC_ERP-new/user-Model/log_login.php
@@ -6,32 +6,35 @@
 		<link rel="stylesheet" href="../view/indexStyle.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	</head>
+		<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+		<link rel="stylesheet" href="sweetalert/dist/sweetalert.css">
+		<script src="sweetalert/dist/sweetalert-dev.js"></script>
 	<style>
 		fieldset{
 			font-family:Microsoft JhengHei;
 			width:20%;
 			margin:auto;
-			padding:20px;
-		}
-
-		.error{
-			color:red;
+			padding:20px;}
+		.confirm{
+			background-color: #FFBB66;
 		}
 	</style>
+	</head>
 	<body>
 		<div class="top">
 		<a href="user_index.php">
 			<img  class="titleImg" src="http://www.lctech.com.tw/wp-content/uploads/bfi_thumb/%E6%9C%AA%E5%91%BD%E5%90%8D-1-%E6%8B%B7%E8%B2%9D-31w65e1o8bhj94p9q5patm@2x.png" width="50" height="50">
 		</a>
 	</div>
-	<div >
+	<div>
+		<?php
+			if(($_GET['msg'])!=""){?>
+					<script>
+					swal.setDefaults({ confirmButtonColor: '#FFBB66' });
+					swal("請輸入正確的帳號及密碼!");
+					</script>
+			<?php } ?>
 		<form method="post" action="../Controller/log_checkUser.php">
-			<?php
-				if(isset($_GET['msg'])){
-					echo "<p class='error'>{$_GET['msg']}</p>";
-				}
-			?>
 			<fieldset>
 			<legend>員工登入</legend>
 			<div>
@@ -44,5 +47,6 @@
 			</fieldset>
 		</form>
 	</div>
+
 	</body>
 </html>
