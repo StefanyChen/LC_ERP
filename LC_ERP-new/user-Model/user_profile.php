d61f4b8c6a9481839616aea5a03c02fd1b4c6be4
David Hsieh <davidhsieh7988@gmail.com>
2017-08-11 10:42:24 +0800

    profile template update
    profile template update

--- LC_ERP-new/user-Model/user_profile.php
+++ LC_ERP-new/user-Model/user_profile.php
@@ -29,27 +29,58 @@ if ($_SESSION['id']== NULL)
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<style>
+ 		.profile-block{
+ 			
+			/*border: solid;*/
+			/*height: 1000px;*/
+			width: 90%;
+			margin:auto;
+			margin-top:30px; 
+ 		}
+ 		.p_sidebar{
+ 			display: inline-block;
+ 			position: relative;
+ 			/*border: solid;*/
+			width:30%;
+			/*height:300px; */
+			
+ 		}
+ 		.p_sidebar_intro{
+ 			display: inline-block;
+ 			/*position: relative;*/
+ 			/*border: solid;*/
+			width:30%;
+			/*height:500px; */
+			
+ 		}
+ 		.p_content{
+ 			float: right;
+ 			width:65% ;
+			/*display: inline-block;*/
+ 			/*border: solid;*/
+			/*height:300px; */
+
+ 		}
+ 		.p_img{
+ 			/*margin:auto;*/
+ 			height:250px ;
+ 			width:220px ;
+ 		}
  		table{
-   		/*border: 1px solid black;*/
-   		width:90% ;
-   		margin:auto;
-   		margin-top:30px;
-		}
-		th{
-		padding:0;
-		margin:0;
-		background-color:#F4A460;
-		text-align:center;
-		font-size:20px;
-		line-height:40px ;
-		}
-		td{
-		text-align: left;
-		}
-		tr{
-			height:38px;
-		}
-  </style>
+ 			width:90%;
+ 			border:0px;
+ 		}
+ 		tr,th,td{
+ 			border:0px;
+ 			padding:0 0 0 0;
+ 			text-align:left;
+ 		}
+ 		p{
+ 			margin-top:10px;
+ 			margin-bottom:10px;
+ 		}
+ 		
+	</style>
 </head>
 <body>
   <div class="top">
@@ -119,111 +150,110 @@ if ($_SESSION['id']== NULL)
 			<div class="right-top">
 				<p class="right-top-title">個人資料</p>
 			</div>
-			<div class="right-down">
-				<table>
+			<div class="right-bottom">
+				<div class="profile-block">
 					<?php
 						while($row = mysqli_fetch_array($result)) {
 					?>
-					<tr>
-						<th colspan="6">員工基本資料</th>
-					</tr>
-					<tr>
-						<td colspan="2"><i class="fa fa-star" style="font-size:17px;color:red"></i>人員編號：<?php echo $row["e_sn"];?></td>
-						<td colspan="2"><i class="fa fa-star" style="font-size:17px;color:red"></i>到職日期：<?php echo $row["e_date"];?></td>
-	<!-- 人員大頭貼  --><td colspan="2" rowspan="4"></td>
-					</tr>
-					<tr>
-						<td colspan="2"><i class="fa fa-star" style="font-size:17px;color:red"></i>中文姓名：<?php echo $row["e_name_cn"];?></td>
-						<td><i class="fa fa-star" style="font-size:17px;color:red"></i>性別：<?php echo $row["e_sex"];?></td>
-						<td ><i class="fa fa-star" style="font-size:17px;color:red"></i>生日：<?php echo $row["e_birth"];?></td>
-					</tr>
-					<tr>
-						<td colspan="2"><i class="fa fa-star" style="font-size:17px;color:red"></i>英文姓名：<?php echo $row["e_name_en"];?></td>
-						<td colspan="2"><i class="fa fa-star" style="font-size:17px;color:red"></i>ID：<?php echo $row["e_personalID"];?></td>
-					</tr>
-					<tr>
-						<td colspan="2"><i class="fa fa-star" style="font-size:17px;color:red"></i>婚姻：<?php echo $row["e_marriage"];?></td>
-						<td colspan="2"><i class="fa fa-star" style="font-size:17px;color:red"></i>血型：<?php echo $row["e_blood"];?></td>
-					</tr>
-					<tr>
-						<td colspan="3"><i class="fa fa-star" style="font-size:17px;color:red"></i>通訊地址：<?php echo $row["e_address"];?></td>
-						<td colspan="3"><i class="fa fa-star" style="font-size:17px;color:red"></i>郵件信箱：<?php echo $row["e_email"];?></td>
-					</tr>
-					<tr>
-						<td colspan="3"><i class="fa fa-star" style="font-size:17px;color:red"></i>緊急聯絡人：<?php echo $row["e_emergency"];?></td>
-						<td colspan="3"><i class="fa fa-star" style="font-size:17px;color:red"></i>緊急聯絡人手機：<?php echo $row["e_em_mobile"];?></td>
-					</tr>
-					<tr>
-						<th colspan="6" style=" ;"><i class="fa fa-star" style="font-size:17px;color:red"></i>學歷</th>
-					</tr>
+					<div class="p_sidebar">
+						<div class="p_img">
+						<img  src="http://www.freeiconspng.com/uploads/profile-icon-9.png" height="250" width="220">
+						</div><br>
+						<hr noshade  color="#A9A9A9" align="left" width="80%">
+					</div>
+					<div class="p_content"><br><br><br>
+						<p>到職日期：<?php echo $row["e_date"];?></p>
+						<p>郵件信箱：<?php echo $row["e_email"];?></p>
+						<p>通訊地址：<?php echo $row["e_address"];?></p>
+						<p>緊急聯絡人：<?php echo $row["e_emergency"];?></p>
+						<p>緊急聯絡人手機：<?php echo $row["e_em_mobile"];?></p>
+						<!-- <br><br><br><hr noshade align="left"> -->
+					</div>
+					
+					<div class="p_content">
+						<table>
 						<tr>
-						<th style="font-size:15px;background-color:#F5F5F5; ">最高學歷</th>
-						<th style="font-size:15px;background-color:#F5F5F5; ">學校</th>
-						<th style="font-size:15px;background-color:#F5F5F5; ">科系</th>
-						<th colspan="2" style="font-size:15px;background-color:#F5F5F5; ">期間</th>
-						<th colspan="2" style="font-size:15px;background-color:#F5F5F5; ">畢/肄/在學</th>
-					</tr>
-<!-- 學歷表格  -->	<tr>
-						<td ><?php echo $row["e_edu"];?></td>
-						<td ><?php echo $row["e_edu_high"];?></td>
-						<td ><?php echo $row["e_edu_dep"];?></td>
-						<td ><?php echo $row["e_edu_start"]?></td >
-						<td ><?php echo $row["e_edu_end"]?></td >
-						<td ><?php echo $row["e_edu_gra"];?></td>
-					</tr>
-					<tr>
-						<th colspan="6"><i class="fa fa-star" style="font-size:17px;color:red"></i>經歷</th>
-					</tr>
-					<tr>
-						<th style="font-size:15px;background-color:#F5F5F5; ">公司名稱</th>
-						<th style="font-size:15px;background-color:#F5F5F5; ">職稱</th>
-						<th colspan="2" style="font-size:15px;background-color:#F5F5F5; ">期間</th>
-						<th colspan="2" style="font-size:15px;background-color:#F5F5F5; ">離職原因</th>
-					</tr>
-<!-- 經歷表格1  -->	<tr>
-
+						<th colspan="6" style="text-align:left">學歷</th>
+						</tr>
+						<tr>
+						<th style="font-size:15px;background-color:#A9A9A9;width:15%  "><?php echo $row["e_edu"];?></th>
+						
+						<th style="font-size:15px;background-color:#D3D3D3 "><?php echo $row["e_edu_high"];?></th>
+						<th style="font-size:15px;background-color:#A9A9A9"><?php echo $row["e_edu_dep"];?></th>
+						<th style="font-size:15px;background-color:#D3D3D3"><?php echo $row["e_edu_start"];?></th>
+						<th style="font-size:15px;background-color:#A9A9A9"><?php echo $row["e_edu_end"]?></th>
+						<th colspan="2" style="font-size:15px;background-color:#D3D3D3"><?php echo $row["e_edu_gra"];?></th>
+					
+						</table>
+						<br><br><br>
+						<!-- <hr noshade align="left"> -->
+					</div>
+					<div class="p_content">
+						<table>
+						<tr>
+						<th colspan="6" style="text-align:left">經歷</th>
+						</tr>
+						<tr>
+						<th style="font-size:15px;background-color:#A9A9A9;width:130px ">公司名稱</th>
+						<th style="font-size:15px;background-color:#D3D3D3;width:130px ">職稱</th>
+						<th colspan="2" style="font-size:15px;background-color:#A9A9A9; ">期間</th>
+						<th colspan="2" style="font-size:15px;background-color:#D3D3D3">離職原因</th>
+						</tr>
+<!-- 經歷表格1  -->	
+						<tr>
 						<td ><?php echo $row["e_exp_com1"];?></td>
 						<td ><?php echo $row["e_exp_posi1"];?></td>
 						<td ><?php echo $row["e_exp_start1"];?></td>
 						<td ><?php echo $row["e_exp_end1"];?></td>
 						<td colspan="2"><?php echo $row["e_exp_reason1"];?></td>
-					</tr>
-					<tr>
-<!-- 經歷表格2  -->						<td ><?php echo $row["e_exp_com2"];?></td>
+						</tr>
+						<tr>
+<!-- 經歷表格2  -->		<td ><?php echo $row["e_exp_com2"];?></td>
 						<td ><?php echo $row["e_exp_posi2"];?></td>
 						<td ><?php echo $row["e_exp_start2"];?></td>
 						<td ><?php echo $row["e_exp_end2"];?></td>
 						<td colspan="2"><?php echo $row["e_exp_reason2"];?></td>
-					</tr>
-					<tr>
+						</tr>
+						<tr>
 <!-- 經歷表格3  -->		<td ><?php echo $row["e_exp_com3"];?></td>
 						<td ><?php echo $row["e_exp_posi3"];?></td>
 						<td ><?php echo $row["e_exp_start3"];?></td>
 						<td ><?php echo $row["e_exp_end3"];?></td>
 						<td colspan="2"><?php echo $row["e_exp_reason3"];?></td>
-					</tr>
-					<tr>
+						</tr>
+						<tr>
 <!-- 經歷表格4  -->		<td ><?php echo $row["e_exp_com4"];?></td>
 						<td ><?php echo $row["e_exp_posi4"];?></td>
 						<td ><?php echo $row["e_exp_start4"];?></td>
 						<td ><?php echo $row["e_exp_end4"];?></td>
 						<td colspan="2"><?php echo $row["e_exp_reason4"];?></td>
-					</tr>
-					<tr>
+						</tr>
+						<tr>
 <!-- 經歷表格5  -->		<td ><?php echo $row["e_exp_com5"];?></td>
 						<td ><?php echo $row["e_exp_posi5"];?></td>
 						<td ><?php echo $row["e_exp_start5"];?></td>
 						<td ><?php echo $row["e_exp_end5"];?></td>
 						<td colspan="2"><?php echo $row["e_exp_reason5"];?></td>
-					</tr>
-					<tr>
-						<th colspan="6"><i class="fa fa-star" style="font-size:17px;color:red"></i>技能專長</th>
-					</tr>
-					<tr>
-						<td colspan="6"><i class="fa fa-star" style="font-size:17px;color:red"></i>認證資格:<?php echo $row["e_license"];?></td>
-					</tr>
+						</tr>
+						</table>
+					</div>
+					<div class="p_sidebar_intro">
+						<p>員工編號：<?php echo $row["e_sn"];?></p>
+						<p>姓名：<?php echo $row["e_name_cn"];?></p>
+						<p>英文名：<?php echo $row["e_name_en"];?></p>
+						<p>生日：<?php echo $row["e_birth"];?></p>
+						<p>身分證：<?php echo $row["e_personalID"];?></p>
+						<p>性別：<?php echo $row["e_sex"];?></p>
+						<p>婚姻：<?php echo $row["e_marriage"];?></p>
+						<p>血型：<?php echo $row["e_blood"];?></p>
+						<p>手機：<?php echo $row["e_mobile"];?></p>
+						<hr noshade color="#A9A9A9" align="left" width="80%">
+						<p style="background-color:#C0C0C0;width:25%;text-align: center  ">技能專長</p>
+						<p><?php echo $row["e_license"];?></p>
+						
+					</div>
 					<?php } ?>
-				</table>
+				</div>
 			</div>
 
 		</div>
