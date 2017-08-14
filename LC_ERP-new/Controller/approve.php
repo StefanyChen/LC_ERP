79406298f41ee93d901d5c72fb93071843da171a
eva20119 <ah13441673@yahoo.com.tw>
2017-08-14 16:56:29 +0800

    通知系統

--- LC_ERP-new/Controller/approve.php
+++ LC_ERP-new/Controller/approve.php
@@ -4,8 +4,10 @@ $username = "root";
 $password = "root";
 $dbname = "peopleresource";
 $reason=$_GET['reason'];
+$name=$_GET['name'];
 try {
     $conn = mysqli_connect($servername,$username ,$password,$dbname);
+    
 
     switch ($_GET['table']) {
       case 'btrip':
@@ -13,19 +15,32 @@ try {
           case 'hr':
             if($_GET['yesNO']=='yes'){
               $sql = "UPDATE `business` SET b_hrCheck='通過' WHERE id={$_GET['id']}";
+              mysqli_query($conn,$sql);
+
+              $sql="UPDATE `message` SET businessMsg='你的出差申請人資已通過' WHERE name='$name'";
+              $result=mysqli_query($conn,$sql);
             }
             else{
               $sql = "UPDATE `business` SET b_hrCheck='不通過',b_bossCheck=' ',b_reason='$reason' WHERE id={$_GET['id']}";
+              $result=mysqli_query($conn,$sql);
+              $sql="UPDATE `message` SET returnBusinessMsg='你的出差申請人資已退件', businessMsg='' WHERE name='$name'";
+              $result=mysqli_query($conn,$sql);
 
             }
             break;
           case 'boss':
             if($_GET['yesNO']=='yes'){
               $sql = "UPDATE `business` SET b_bossCheck='通過' WHERE id={$_GET['id']}";
+              $result=mysqli_query($conn,$sql);
+              $sql="UPDATE `message` SET businessMsg='你的出差申請老闆已通過' WHERE name='$name'";
+              $result=mysqli_query($conn,$sql);
             }
             else{
 
               $sql = "UPDATE `business` SET b_bossCheck='不通過',b_reason='$reason' WHERE id={$_GET['id']}";
+              $result=mysqli_query($conn,$sql);
+              $sql="UPDATE `message` SET returnBusinessMsg='你的出差申請老闆已退件', businessMsg='' WHERE name='$name'";
+              $result=mysqli_query($conn,$sql);
             }
             break;
           default:
@@ -39,17 +54,30 @@ try {
           case 'hr':
             if($_GET['yesNO']=='yes'){
               $sql = "UPDATE `leave` SET l_hrCheck='通過' WHERE id={$_GET['id']}";
+              mysqli_query($conn,$sql);
+              $sql="UPDATE `message` SET leaveMsg='你的請假申請人資已通過' WHERE name='$name'";
+              
+              $result=mysqli_query($conn,$sql);
             }
             else{
               $sql = "UPDATE `leave` SET l_hrCheck='不通過',l_bossCheck=' ',l_reason='$reason' WHERE id={$_GET['id']}";
+              $result=mysqli_query($conn,$sql);
+              $sql="UPDATE `message` SET returnLeaveMsg='你的請假申請人資已退件', leaveMsg='' WHERE name='$name'";
+              $result=mysqli_query($conn,$sql);
             }
             break;
           case 'boss':
-            if($_GET['a']=='yes'){
+            if($_GET['yesNO']=='yes'){
               $sql = "UPDATE `leave` SET l_bossCheck='通過' WHERE id={$_GET['id']}";
+              $result=mysqli_query($conn,$sql);
+              $sql="UPDATE `message` SET leaveMsg='你的請假申請老闆已通過' WHERE name='$name'";
+              $result=mysqli_query($conn,$sql);
             }
             else{
               $sql = "UPDATE `leave` SET l_bossCheck='不通過', l_reason='$reason' WHERE id={$_GET['id']}";
+               mysqli_query($conn,$sql);
+              $sql="UPDATE `message` SET returnLeaveMsg='你的請假申請老闆已退件', leaveMsg='' WHERE name='$name'";
+              $result=mysqli_query($conn,$sql);
             }
             break;
           default:
@@ -63,17 +91,29 @@ try {
           case 'hr':
             if($_GET['yesNO']=='yes'){
               $sql = "UPDATE `overtime_apply` SET o_hrCheck='通過' WHERE id={$_GET['id']}";
+              mysqli_query($conn,$sql);
+              $sql="UPDATE `message` SET overTimeMsg='你的加班申請人資已通過' WHERE name='$name'";
+              mysqli_query($conn,$sql);
             }
             else{
               $sql = "UPDATE `overtime_apply` SET o_hrCheck='不通過',o_bossCheck=' ' WHERE id={$_GET['id']}";
+              $result=mysqli_query($conn,$sql);
+              $sql="UPDATE `message` SET returnOvertimeMsg='你的加班申請人資已退件', overTimeMsg='' WHERE name='$name'";
+              $result=mysqli_query($conn,$sql);
             }
             break;
           case 'boss':
-            if($_GET['a']=='yes'){
+            if($_GET['yesNO']=='yes'){
               $sql = "UPDATE `overtime_apply` SET o_bossCheck='通過' WHERE id={$_GET['id']}";
+              $result=mysqli_query($conn,$sql);
+              $sql="UPDATE `message` SET overTimeMsg='你的加班申請老闆已通過' WHERE name='$name'";
+              $result=mysqli_query($conn,$sql);
             }
             else{
               $sql = "UPDATE `overtime_apply` SET o_bossCheck='不通過' WHERE id={$_GET['id']}";
+              $result=mysqli_query($conn,$sql);
+              $sql="UPDATE `message` SET returnOvertimeMsg='你的加班申請老闆已退件', overTimeMsg='' WHERE name='$name'";
+              $result=mysqli_query($conn,$sql);
             }
             break;
           default:
